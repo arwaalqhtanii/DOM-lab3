f887474b84c0676b07c1a3a44fb3de0508df3ec9
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-12 11:02:00 +0300

    code write

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+button1 = document.getElementById('button1');
+button2 = document.getElementById('button2');
+button3 = document.getElementById('button3');
+button1.classList.add('button1');
+button2.classList.add('button2');
+button3.classList.add('button3');
